9f61359c0dec7fe2eb04b899debdc5770552156a
Hrocha1109 <69353759+Hrocha1109@users.noreply.github.com>
2020-08-07 11:13:13 -0300

    Dados update

--- index.js
+++ index.js
@@ -110,3 +110,21 @@ bot.on('message', msg => {
         }
     }
 })
+
+bot.on('message', msg =>{
+    var sorte = msg.content
+    if(!msg.author.bot){
+        return;
+    }
+    switch(sorte){
+        case sorte > 1 && sorte <= 10: msg.channel.send('CARALHO');
+    
+    break;
+        case sorte = 1: msg.channel.send('CRITICO!!!!');
+    break;
+        case sorte > 85 && sorte !=100: msg.channel.send('Puts...');
+     break;
+        case sorte = 100: msg.channel.send('P U T S')
+
+    }
+})
